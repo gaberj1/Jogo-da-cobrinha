9364f3ea9341dcca883dd48d5e13bac2f721cb5d
gaberj1 <pereirasilva169@outlook.com>
2025-12-10 10:25:31 -0300

    teste do easter egg

--- tela.js
+++ tela.js
@@ -1,9 +1,21 @@
 const tela = {
     largura:800,
     altura:400,
-    cor:"green",
-    desenhar(){
-        cCtx.fillStyle= this.cor;
-        cCtx.fillRect(0,60,this.largura,this.altura);
+    cor:""#286047"",
+	esterEgg:true,
+    desenhar: function(){
+        canvasCtx.fillStyle= this.cor;
+        canvasCtx.fillRect(0,60,this.largura,this.altura);
+		if (this.esterEgg){	
+			canvasCtx.fillStyle= "purple"; 
+            canvasCtx.font= "30px Fantasy";
+            canvasCtx.textBaseline = "top";
+            canvasCtx.textAlign = "center";
+            canvasCtx.fillText("Você achou o Easter EGG !",400,160);
+            img = new Image();
+            img.src = 'imagem.png';
+            canvasCtx.drawImage(img, 300, 200,200,200);
+		}
+		
     }
-}
+};

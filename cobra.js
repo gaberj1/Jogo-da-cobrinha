244079ff3d0f1297f609c4d830f6bd521f8d801a
gaberj1 <pereirasilva169@outlook.com>
2025-12-10 10:40:02 -0300

    aaaaa

--- cobra.js
+++ cobra.js
@@ -1,34 +1,92 @@
 const cobra = {
-    vida: 3,
+    x:[100],
+    y:[100],
     tamanho:10,
-    x:400,
-    y:260,
-    cor:'brown',
-    direcao:'right',
+    comprimento:10,
+    taxaCrescimento:100,
+    cor:"black",
+    direcao:0,
+    vida:3,
+	esterEgg:false,
     desenhar(){
-        cCtx.fillStyle = this.cor;
-        cCtx.fillRect(this.x,this.y,this.tamanho,this.tamanho);
+        canvasCtx.fillStyle= this.cor;
+        for (let i = 0; i < this.x.length; ++i)
+                canvasCtx.fillRect(this.x[i],this.y[i],this.tamanho,this.tamanho);
     },
-    mover(){
-      if (this.direcao == "right")this.x++;
-
-      if (this.direcao == "left")this.x--;
-
-      if (this.direcao == "down")this.y++;
-      
-      if (this.direcao == "up")this.y--;
-      
-      if((this.x >= tela.largura - this.tamanho) || (this.x <= 0)) 
-      this.morrer();
-
-      if((this.y >= tela.altura + placar.altura - this.tamanho) || (this.y <= placar.altura)) 
-      this.morrer();
-
-      
+    easterEgg(){
+        tela.cor = "#E6E6FA"
+        tela.esterEgg = true;
+		placar.corFundo = "purple"
+		this.cor = "#9932CC"
+        this.x=[20]
+        this.y=[70]
+	},
+	mover(){
+        if (this.direcao == 0){
+                this.x.unshift(this.x[0]+1)
+                this.y.unshift(this.y[0])
+                this.x.pop()
+                this.y.pop()
+        }
+        if (this.direcao == 90){
+                this.x.unshift(this.x[0])
+                this.y.unshift(this.y[0]+1)
+                this.x.pop()
+                this.y.pop()
+        }
+        if (this.direcao == 180){
+                this.x.unshift(this.x[0]-1)
+                this.y.unshift(this.y[0])
+                this.x.pop()
+                this.y.pop()
+        }
+        if (this.direcao == 270){
+                this.x.unshift(this.x[0])
+                this.y.unshift(this.y[0]-1)
+                this.x.pop()
+                this.y.pop()
+        }
+		if ((this.vida == 2) && 
+            (this.x[0] > tela.largura - 20 ) && 
+            (this.y[0] < placar.altura + 10) &&
+            (placar.qtdPontos > 15))
+            this.easterEgg()
+		
+        if ((this.x[0] > tela.largura - this.tamanho)
+           ||  (this.x[0] < 0)
+           ||  (this.y[0] < placar.altura)
+           ||  (this.y[0] > placar.altura + tela.altura - this.tamanho)
+           ||  (this.seComeu()))
+                this.morrer()
     },
-morrer(){
-  this.vida--;
-  this.x=400;
-  this.y=260;
-}
-}
+    morrer(){
+        this.vida--;
+        this.x=[400];
+        this.y=[260];
+        if (this.vida==2) this.cor="orange"
+        if (this.vida==1) this.cor="red"
+    }, 
+    crescer()
+    {
+        for (let i = 0; i < this.taxaCrescimento; ++i){
+            this.x.unshift(this.x[0]);
+            this.y.unshift(this.y[0]);
+        }
+        this.comprimento+=this.taxaCrescimento;
+    },
+    seComeu(){     
+        if (this.x.length > 1)
+                if ((this.x[this.tamanho] != this.x[this.tamanho+1]) || (this.y[this.tamanho] != this.y[this.tamanho+1]))
+                        for (let i = 3*this.tamanho; i < this.x.length ; ++i){
+                                const distX = this.x[i] - this.x[0];
+                                const distY = this.y[i] - this.y[0];
+                                if ((Math.abs(distX) < (this.tamanho-5)) 
+                                    && (Math.abs(distY) < (this.tamanho-5)))
+                                       return true;      
+                                 
+                }                      
+        return false;                    
+    }
+};
+
+
